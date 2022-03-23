794598c987831544af9dc3dce200dfed8a372de0
Koyo Nakamura <koyonakamura123@gmail.com>
2022-03-23 16:32:21 -0700

    main functionality complete - need to work on UI

--- src/AdminView.tsx
+++ src/AdminView.tsx
@@ -40,26 +40,54 @@ function AdminView() {
         setState([...newState]);
     }
 
+    // sends POST request to kazumirecipeapi to add recipe to database
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        let form: HTMLFormElement = document.querySelector('form')!;
+        let formData = new FormData(form);
+        // clean ingredients and directions to remove empty last element
+        removeLast('ingredients', formData);
+        removeLast('directions', formData);
+        console.log(formData.getAll('ingredients'));
+        fetch('https://kazumirecipeapi.uw.r.appspot.com/addRecipe', {
+            method: 'POST',
+            body: formData
+        }).then(res => res.json()).then(handleResponse).catch(console.error);
+    }
+
+    // updates page to notify that the recipe has been added or rejected
+    function handleResponse(json: any) {
+        console.log(json);
+    }
+
+    function removeLast(category: string, formData: FormData) {
+        let original = formData.getAll(category);
+        formData.delete(category);
+        for(let i = 0; i < original.length - 1; i++) {
+            formData.append(category, original[i]);
+        }
+    }
+
     return (
         <Box sx={formStyle}>
             <Typography id="recipe-form-title" variant="h4" component="h2" sx={{marginTop: '5rem', marginBottom: '2rem'}}>
                 Add Recipe
             </Typography>
-            <form method='POST' style={{ display: 'flex', flexDirection: 'column', gap: '3rem'}}>
-                <TextField required id="name" label="Recipe Name" variant="outlined" />
-                <Input required type="file" id="image" inputProps={{accept: 'image/*'}}/>
+            <form id='add-recipe' method='POST' onSubmit={event => {handleSubmit(event)}} style={{ display: 'flex', flexDirection: 'column', gap: '3rem'}}>
+                <TextField required name="name" label="Recipe Name" variant="outlined" />
+                <Input required type="file" name="image" inputProps={{accept: 'image/*'}}/>
                 <div id='ingredients'>
                     {ingredients.map((value, index) => 
                         index === 0 ?
-                        <TextField required key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, ingredients, setIngredients)}} name="ingredient" label="Add ingredient" variant="outlined" />: 
-                        <TextField key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, ingredients, setIngredients)}} name="ingredient" label="Add ingredient" variant="outlined" />)
+                        <TextField required key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, ingredients, setIngredients)}} name="ingredients" label="Add ingredient" variant="outlined" />: 
+                        <TextField key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, ingredients, setIngredients)}} name="ingredients" label="Add ingredient" variant="outlined" />)
                     }
                 </div>
                 <div id='directions'>
                     {directions.map((value, index) => 
                         index === 0 ?
-                        <TextField required key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, directions, setDirections)}} name="direction" label="Add direction" variant="outlined" />: 
-                        <TextField key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, directions, setDirections)}} name="direction" label="Add direction" variant="outlined" />)
+                        <TextField required key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, directions, setDirections)}} name="directions" label="Add direction" variant="outlined" />: 
+                        <TextField key={index} value={value} onChange={event => {handleTextInputOnChange(event, index, directions, setDirections)}} name="directions" label="Add direction" variant="outlined" />)
                     }
                 </div>
                 <Button type='submit' id='submit' variant="contained">
